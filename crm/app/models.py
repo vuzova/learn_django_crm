4d0b356a177786938bd2b3d47e08f3e9eb2dca84
vuzova <vasilina997@gmail.com>
2023-06-06 01:46:41 +0500

    настройка бутстрап и домашней страницы, работа с моделями

--- crm/app/models.py
+++ crm/app/models.py
@@ -6,6 +6,8 @@ class Articles(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     text = models.TextField(verbose_name='Текст статьи')
 
+    def __str__(self):
+        return self.name
     class Meta:
         verbose_name = 'Статью'
         verbose_name_plural = 'Статьи'
--- crm/app/views.py
+++ crm/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(requst):
-    return render(requst, 'home.html')
+    return render(requst, 'home.html', context= {
+        'list_articles' : Articles.objects.all(),
+        'name': 'User'
+    })
